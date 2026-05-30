50a8a897882cd0d1c474b0550f80558f4b717f6b
shayanJR99 <shahrbandishayan@gmail.com>
2026-05-30 05:39:17 +0330

    shayanJR99: added custom email base user auth and waiting for frontend
    + used accounts app in project
    
    im wating to front dev make a template to conect it to that

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,3 +1,71 @@
+from django import forms
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField, UserCreationForm, UserChangeForm
+from .models import User,Profile
 
-# Register your models here.
+# ۱. فرم ساخت کاربر جدید در ادمین
+class MyUserCreationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput, label='Password')
+
+    class Meta:
+        model = User
+        fields = ('email',)
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
+
+# ۲. فرم ویرایش کاربر در ادمین
+class MyUserChangeForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField(
+        label='Password',
+        help_text="Password hashes help keep your password secure. "
+                  "You can change the password using <a href=\"../password/\">this form</a>."
+    )
+
+    class Meta:
+        model = User
+        fields = '__all__'
+
+# ۳. تنظیمات نهایی پنل ادمین
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    
+    # فرم‌هایی که بالا ساختیم را اینجا معرفی می‌کنیم
+    form = MyUserChangeForm
+    add_form = MyUserCreationForm
+
+    # فیلدهایی که در لیست نمایش داده می‌شوند
+    list_display = ('email', 'is_staff', 'is_verified')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'is_verified')
+    
+    # فیلدهایی که در صفحه ویرایش نمایش داده می‌شوند
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'is_verified', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'created_date', 'updated_date')}),
+    )
+
+    # فیلدهایی که موقع ساخت کاربر نمایش داده می‌شوند
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email','password'),
+        }),
+    )
+
+    search_fields = ('email', )
+    ordering = ('email',)
+    filter_horizontal = ('groups', 'user_permissions')
+    readonly_fields = ('created_date', 'updated_date', 'last_login')
+
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    # فیلدهایی که می‌خواهید برای پروفایل در ادمین نمایش داده شود
+    list_display = ('user', 'first_name','last_name') 
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
-
+from django.contrib.auth.views import LoginView
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from .models import User
+from .forms import SignupForm
+from django.contrib.auth.views import LogoutView
 # Create your views here.
+
+
+# accounts/views.py
+
+from django.contrib.auth.views import LoginView
+from django.views.generic.edit import CreateView
+
+
+class LoginUser(LoginView):
+
+    template_name = "accounts/login.html"
+
+    redirect_authenticated_user = True          
+    
+    
+class LogoutUser(LogoutView):
+
+    next_page = "accounts:login"
+
+    
+class SignupUser(CreateView):
+
+    model = User
+
+    form_class = SignupForm
+
+    template_name = "accounts/signup.html"
+
+    success_url = reverse_lazy(
+        "accounts:login"
+    )
